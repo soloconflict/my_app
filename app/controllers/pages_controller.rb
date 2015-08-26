94ab47ba50f1ca5b9db5c82cb332146213a3c9b7
Nathan Chang <chang1030nathan@gmail.com>
2015-08-26 11:16:12 +0800

    generated pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def service
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/home'
+
+  get 'pages/service'
+
+  get 'pages/about'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
