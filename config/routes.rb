6936377dfbbda63552c03be3d61e08008159c7b4
Nathan Chang <chang1030nathan@gmail.com>
2015-09-14 19:11:27 +0800

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	
-  devise_for :users
+	
+	
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 	resources :contacts
 	
 	root 'pages#index'
